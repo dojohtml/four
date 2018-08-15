c229a87bb1a57e2155ce9738fc00a14cd11a9e73
dojohtml <41997308+dojohtml@users.noreply.github.com>
2018-08-15 07:51:55 -0300

    Add files via upload
    Resolvendo o teste.

--- Aula 5/buzzfeed.js
+++ Aula 5/buzzfeed.js
@@ -1,3 +1,20 @@
 function myFunction() {
-  alert("Você não é um salgado")
+  	var inputs = document.querySelectorAll("input[type='checkbox']");
+  	var soma = 0
+  	for(var i = 0; i < inputs.length; i++) {
+  		if (inputs[i].checked == true){
+	  		soma = soma + parseInt(inputs[i].value)   	
+  		}
+    	
+	}
+	console.log(soma)
+	if(soma <=3){
+	  alert("Coxinha")
+	}else if(soma>=4 && soma<=6 ){
+	  alert("bolinha de queijo")
+	}
+	else{
+	  alert("Kibe")
+
+	}
 }
